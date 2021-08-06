45667ce57a345738d684406e5eee448fe7e54f1d
matbmotta <mat-motta@hotmail.com>
2021-08-06 20:48:03 -0300

    atualizacao

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace exemplo02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Exemplo Github");
+        }
+    }
+}
